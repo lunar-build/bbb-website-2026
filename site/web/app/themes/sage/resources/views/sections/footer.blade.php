884df2119f992ca2a5f25579773e6475beaebfd2
Harry <gardiner_harry@yahoo.com>
2026-08-11 16:16:03 +0100

    Removed footer widget

--- site/web/app/themes/sage/resources/views/sections/footer.blade.php
+++ site/web/app/themes/sage/resources/views/sections/footer.blade.php
@@ -1,13 +1,9 @@
 @php
-  $columns = menu_items_to_footer_columns('footer_navigation');
-  $legal = legal_links_from_options();
-  $copyright = sprintf('© %s %s', date('Y'), get_bloginfo('name'));
+    $columns = menu_items_to_footer_columns('footer_navigation');
+    $legal = legal_links_from_options();
+    $copyright = sprintf('© %s %s', date('Y'), get_bloginfo('name'));
 @endphp
 
-<lunar-site-footer
-  copyright="{{ $copyright }}"
-  columns="{{ json_encode($columns) }}"
-  legal="{{ json_encode($legal) }}"
->
-  <a slot="logo" href="{{ home_url('/') }}">{!! $siteName !!}</a>
+<lunar-site-footer copyright="{{ $copyright }}" columns="{{ json_encode($columns) }}" legal="{{ json_encode($legal) }}">
+    <a slot="logo" href="{{ home_url('/') }}">{!! $siteName !!}</a>
 </lunar-site-footer>
