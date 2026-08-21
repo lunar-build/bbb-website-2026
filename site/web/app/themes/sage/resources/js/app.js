a5f1cee4b01e3d4f958a0d05342d5d906066079e
Harry <gardiner_harry@yahoo.com>
2026-08-21 09:55:12 +0100

    Added base styling.

--- site/web/app/themes/sage/resources/js/app.js
+++ site/web/app/themes/sage/resources/js/app.js
@@ -4,6 +4,7 @@ import '@awesome.me/webawesome/dist/components/card/card.js';
 import '@awesome.me/webawesome/dist/components/accordion/accordion.js';
 import '@awesome.me/webawesome/dist/components/accordion-item/accordion-item.js';
 import '@awesome.me/webawesome/dist/components/icon/icon.js';
+import '@awesome.me/webawesome/dist/components/badge/badge.js';
 import '@lunar.build/lunar-ui-components/main.js';
 import '../styles/app.scss';
 
