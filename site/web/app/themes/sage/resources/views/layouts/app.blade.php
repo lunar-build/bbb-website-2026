f2d8271ccab02e1502f3515814c1b12ce2deaafa
Harry <gardiner_harry@yahoo.com>
2026-08-21 11:24:46 +0100

    Added utils stylesheet. Updated skip link to use new SR only

--- site/web/app/themes/sage/resources/views/layouts/app.blade.php
+++ site/web/app/themes/sage/resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
     @php(wp_body_open())
 
     <div id="app">
-      <a class="sr-only focus:not-sr-only" href="#main">
+      <a class="u-sr-only" href="#main">
         {{ __('Skip to content', 'sage') }}
       </a>
 
