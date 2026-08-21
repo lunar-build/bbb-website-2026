ec6cfc1107ef145726f61a271e4752ee551a51a4
Harry <gardiner_harry@yahoo.com>
2026-08-21 12:19:49 +0100

    Added button, with base styling. Created SVG repo for icons. Can use blade anonymous component call to render x-icon name=arrow-right.

--- site/web/app/themes/sage/resources/js/app.js
+++ site/web/app/themes/sage/resources/js/app.js
@@ -4,6 +4,10 @@ import '@awesome.me/webawesome/dist/components/card/card.js';
 import '@awesome.me/webawesome/dist/components/accordion/accordion.js';
 import '@awesome.me/webawesome/dist/components/accordion-item/accordion-item.js';
 import '@awesome.me/webawesome/dist/components/icon/icon.js';
+import '@awesome.me/webawesome/dist/components/input/input.js';
+import '@awesome.me/webawesome/dist/components/radio/radio.js';
+import '@awesome.me/webawesome/dist/components/radio-group/radio-group.js';
+import '@awesome.me/webawesome/dist/components/checkbox/checkbox.js';
 import '@lunar.build/lunar-ui-components/main.js';
 import '../styles/app.scss';
 
