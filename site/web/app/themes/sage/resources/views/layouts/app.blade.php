5193188fdcee99fa47b06d515d787f608875ed2f
Harry <gardiner_harry@yahoo.com>
2026-08-21 10:38:21 +0100

    Removed Tailwind

--- site/web/app/themes/sage/resources/views/layouts/app.blade.php
+++ site/web/app/themes/sage/resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     @php(do_action('get_header'))
     @php(wp_head())
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/js/app.js'])
   </head>
 
   <body @php(body_class())>
--- site/web/app/themes/sage/vite.config.js
+++ site/web/app/themes/sage/vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite';
 import laravel from 'laravel-vite-plugin'
 import { wordpressPlugin, wordpressThemeJson } from '@roots/vite-plugin';
 
@@ -11,10 +10,8 @@ if (! process.env.APP_URL) {
 export default defineConfig({
   base: '/app/themes/sage/public/build/',
   plugins: [
-    tailwindcss(),
     laravel({
       input: [
-        'resources/css/app.css',
         'resources/js/app.js',
         'resources/css/editor.css',
         'resources/js/editor.js',
@@ -26,12 +23,12 @@ export default defineConfig({
     wordpressPlugin(),
 
     // Generate the theme.json file in the public/build/assets directory
-    // based on the Tailwind config and the theme.json file from base theme folder
+    // from the hand-authored theme.json in the base theme folder
     wordpressThemeJson({
-      disableTailwindColors: false,
-      disableTailwindFonts: false,
-      disableTailwindFontSizes: false,
-      disableTailwindBorderRadius: false,
+      disableTailwindColors: true,
+      disableTailwindFonts: true,
+      disableTailwindFontSizes: true,
+      disableTailwindBorderRadius: true,
     }),
   ],
   resolve: {
