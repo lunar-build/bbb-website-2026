510aee195a27a69beea44cc39a650a149464e8c1
Harry <gardiner_harry@yahoo.com>
2026-08-28 12:43:56 +0100

    CTA banner h2 is styled as h1. Stand first styling applied to text

--- site/web/app/themes/sage/resources/views/blocks/cta-banner.blade.php
+++ site/web/app/themes/sage/resources/views/blocks/cta-banner.blade.php
@@ -2,18 +2,23 @@
   <section {{ $attributes->class(['c-cta-banner', 'c-cta-banner--'.$layout]) }}>
 @endunless
 
-<div class="o-container">
-<div class="c-cta-banner__inner">
-  @if ($layout === 'centred' && $imageLeft)
-    <img class="c-cta-banner__image c-cta-banner__image--left" src="{{ $imageLeft }}" alt="">
-  @endif
+@if ($layout === 'centred' && $imageLeft)
+  <img class="c-cta-banner__image c-cta-banner__image--left" src="{{ $imageLeft }}" alt="">
+@endif
 
+@if ($layout === 'centred' && $imageRight)
+  <img class="c-cta-banner__image c-cta-banner__image--right" src="{{ $imageRight }}" alt="">
+@elseif ($layout === 'left' && $image)
+  <img class="c-cta-banner__image c-cta-banner__image--right" src="{{ $image }}" alt="">
+@endif
+
+<div class="o-container">
   <div class="c-cta-banner__content">
     @if ($heading)
-      <h2 class="c-cta-banner__heading">{{ $heading }}</h2>
+      <h2 class="c-cta-banner__heading u-heading-1">{{ $heading }}</h2>
     @endif
 
-    <p class="c-cta-banner__body">{{ $body }}</p>
+    <p class="c-cta-banner__body u-standfirst">{{ $body }}</p>
 
     <wa-button
       class="c-cta-banner__cta"
@@ -28,13 +33,6 @@
       <x-icon name="arrow-right" slot="end" />
     </wa-button>
   </div>
-
-  @if ($layout === 'centred' && $imageRight)
-    <img class="c-cta-banner__image c-cta-banner__image--right" src="{{ $imageRight }}" alt="">
-  @elseif ($layout === 'left' && $image)
-    <img class="c-cta-banner__image c-cta-banner__image--right" src="{{ $image }}" alt="">
-  @endif
-</div>
 </div>
 
 @unless ($block->preview)
