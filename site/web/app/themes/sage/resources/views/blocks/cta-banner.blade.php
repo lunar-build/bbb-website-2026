152a70b79c8384b1c910a341fcd8fa660efa9a88
Harry <gardiner_harry@yahoo.com>
2026-08-28 12:41:51 +0100

    Updated blade file

--- site/web/app/themes/sage/resources/views/blocks/cta-banner.blade.php
+++ site/web/app/themes/sage/resources/views/blocks/cta-banner.blade.php
@@ -1,26 +1,40 @@
 @unless ($block->preview)
-  <section {{ $attributes->class(['c-cta-strip']) }}>
+  <section {{ $attributes->class(['c-cta-banner', 'c-cta-banner--'.$layout]) }}>
 @endunless
 
 <div class="o-container">
-<wa-card class="c-cta-strip__card" appearance="outlined" with-footer>
-  <div class="c-cta-strip__body">
-    <InnerBlocks template="{{ $block->template }}" />
+<div class="c-cta-banner__inner">
+  @if ($layout === 'centred' && $imageLeft)
+    <img class="c-cta-banner__image c-cta-banner__image--left" src="{{ $imageLeft }}" alt="">
+  @endif
+
+  <div class="c-cta-banner__content">
+    @if ($heading)
+      <h2 class="c-cta-banner__heading">{{ $heading }}</h2>
+    @endif
+
+    <p class="c-cta-banner__body">{{ $body }}</p>
+
+    <wa-button
+      class="c-cta-banner__cta"
+      variant="brand"
+      appearance="accent"
+      pill
+      with-end
+      href="{{ $link['url'] }}"
+      @if (($link['target'] ?? '') === '_blank') target="_blank" rel="noopener" @endif
+    >
+      {{ $link['title'] }}
+      <x-icon name="arrow-right" slot="end" />
+    </wa-button>
   </div>
 
-  <wa-button
-    slot="footer"
-    variant="brand"
-    appearance="accent"
-    pill
-    with-end
-    href="{{ $link['url'] }}"
-    @if (($link['target'] ?? '') === '_blank') target="_blank" rel="noopener" @endif
-  >
-    {{ $link['title'] }}
-    <x-icon name="arrow-right" slot="end" />
-  </wa-button>
-</wa-card>
+  @if ($layout === 'centred' && $imageRight)
+    <img class="c-cta-banner__image c-cta-banner__image--right" src="{{ $imageRight }}" alt="">
+  @elseif ($layout === 'left' && $image)
+    <img class="c-cta-banner__image c-cta-banner__image--right" src="{{ $image }}" alt="">
+  @endif
+</div>
 </div>
 
 @unless ($block->preview)
