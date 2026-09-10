8e6dc8e59e5aa32b52e14787da31e500bd5d4282
Harry <gardiner_harry@yahoo.com>
2026-09-10 12:21:38 +0100

    Tidy conflict left over code. list-style none for all header lists

--- site/web/app/themes/sage/resources/views/sections/primary-nav.blade.php
+++ site/web/app/themes/sage/resources/views/sections/primary-nav.blade.php
@@ -2,28 +2,24 @@
      L2 headings are inert, L3 items are the real links. --}}
 <div id="primary-menu" class="c-primary-nav" hidden>
     <div class="c-primary-nav__panel" data-panel="root">
-        <<<<<<< HEAD <ul class="c-primary-nav__list" role="list">
-            =======
-            <ul class="c-primary-nav__list"
-                aria-label="{{ wp_get_nav_menu_name('primary_navigation') ?: __('Primary', 'sage') }}">
-                >>>>>>> nav
-                @foreach ($items as $i => $item)
-                    <li class="c-primary-nav__item">
-                        @if (!empty($item['children']))
-                            <button type="button" class="c-primary-nav__link c-primary-nav__link--drill"
-                                data-open-panel="l1-{{ $i }}">
-                                <span>{{ $item['label'] }}</span>
-                                <x-icon name="chevron" class="c-primary-nav__chevron" />
-                            </button>
-                        @else
-                            <a href="{{ $item['href'] }}"
-                                class="c-primary-nav__link"><span>{{ $item['label'] }}</span></a>
-                        @endif
-                    </li>
-                @endforeach
-            </ul>
+        <ul class="c-primary-nav__list"
+            aria-label="{{ wp_get_nav_menu_name('primary_navigation') ?: __('Primary', 'sage') }}">
+            @foreach ($items as $i => $item)
+                <li class="c-primary-nav__item">
+                    @if (!empty($item['children']))
+                        <button type="button" class="c-primary-nav__link c-primary-nav__link--drill"
+                            data-open-panel="l1-{{ $i }}">
+                            <span>{{ $item['label'] }}</span>
+                            <x-icon name="chevron" class="c-primary-nav__chevron" />
+                        </button>
+                    @else
+                        <a href="{{ $item['href'] }}" class="c-primary-nav__link"><span>{{ $item['label'] }}</span></a>
+                    @endif
+                </li>
+            @endforeach
+        </ul>
 
-            @include('partials.primary-nav-panel-footer', ['cta' => $cta, 'socialLinks' => $socialLinks])
+        @include('partials.primary-nav-panel-footer', ['cta' => $cta, 'socialLinks' => $socialLinks])
     </div>
 
     @foreach ($items as $i => $item)
