8f6d723cacab25d25e31b1f3f0cc138fb93ada5b
Harry <gardiner_harry@yahoo.com>
2026-09-10 16:15:42 +0100

    Wrapped find_nearby in form.

--- site/web/app/themes/sage/resources/views/blocks/journey-planner-widget.blade.php
+++ site/web/app/themes/sage/resources/views/blocks/journey-planner-widget.blade.php
@@ -9,25 +9,27 @@
   <h2 class="c-journey-planner-widget__heading">{{ $heading }}</h2>
 
   @if ($variant === 'find_nearby')
-    <x-input
-      label="Your location"
-      name="location"
-      placeholder="Enter a postcode or address…"
-      icon="send"
-      class="c-journey-planner-widget__field"
-    />
+    <form class="c-journey-planner-widget__form" data-variant="find_nearby">
+      <x-input
+        label="Your location"
+        name="location"
+        placeholder="Enter a postcode or address…"
+        icon="send"
+        class="c-journey-planner-widget__field"
+      />
 
-    <div class="c-journey-planner-widget__options">
-      @foreach ($nearbyOptions as $i => $option)
-        <x-radio name="nearby_feature" label="{{ $option['label'] }}" :checked="$i === 0" />
-      @endforeach
-    </div>
+      <div class="c-journey-planner-widget__options">
+        @foreach ($nearbyOptions as $i => $option)
+          <x-radio name="nearby_feature" label="{{ $option['label'] }}" :checked="$i === 0" />
+        @endforeach
+      </div>
 
-    {{-- TODO: wire up the "find nearby" redirect once the real endpoint/param shape is confirmed. --}}
-    <wa-button variant="neutral" appearance="accent" pill with-end disabled class="c-journey-planner-widget__cta">
-      {{ $ctaLabel }}
-      <x-icon name="arrow-right" slot="end" />
-    </wa-button>
+      {{-- TODO: wire up the "find nearby" redirect once the real endpoint/param shape is confirmed. --}}
+      <wa-button variant="neutral" appearance="accent" pill with-end disabled class="c-journey-planner-widget__cta">
+        {{ $ctaLabel }}
+        <x-icon name="arrow-right" slot="end" />
+      </wa-button>
+    </form>
   @else
     <form class="c-journey-planner-widget__form" data-variant="plan_route" data-redirect-url="{{ $journeyPlannerUrl }}">
       <x-input
