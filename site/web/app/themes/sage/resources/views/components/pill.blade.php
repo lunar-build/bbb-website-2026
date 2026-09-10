d6ed45b33414a070f9a60dac926234b0c0bcc755
Harry <gardiner_harry@yahoo.com>
2026-09-10 17:01:08 +0100

    Reworked pill as per new design

--- site/web/app/themes/sage/resources/views/components/pill.blade.php
+++ site/web/app/themes/sage/resources/views/components/pill.blade.php
@@ -8,7 +8,6 @@
 @if ($url)
     <a {{ $attributes->class(['c-pill']) }} href="{{ $url }}">
         {{ $label }}
-        <x-icon name="arrow-right" class="c-pill__icon" />
     </a>
 @else
     <span {{ $attributes->class(['c-pill']) }}>
